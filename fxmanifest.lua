917822784fc36fc578cfc1c159f73cccd1033740
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-18 21:55:33 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+
+description "Razed Give Cash"
+
+author 'planeklm'
+
+version '1.0'
+
+lua54 'yes'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua'
