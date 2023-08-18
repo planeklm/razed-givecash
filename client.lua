e58879162294532139e6e7d0bace87bf3bd41278
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-18 23:48:16 +0100

    Added main input menu and choice of targets

--- client.lua
+++ client.lua
@@ -1,18 +1,41 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+RegisterNetEvent('moneyMenu', function()
+    lib.inputDialog('Money Menu', {
+        {type = 'number', label = 'ID of recipent', description = 'The ID of the recipent.', default = '1', required = true, icon = 'user'},
+        {type = 'number', label = '$ Amount', description = 'The amount of money to send to the recipent.', default = '10', required = true, icon = 'money'}
+    })
+    
+    if not input then return end
+
+end)
 
 
 CreateThread(function()
+    if Config.Target == 'qb' then
     exports['qb-target']:AddGlobalPlayer({
         options = {
             { 
                 action = function()
-                    cashMenu()
+                    moneyMenu()
                 end,
                 icon = "fas fa-money",
-                label = "Give Cash",
+                label = "Give Money",
             },
         },
         distance = 3.0 
+    })
+else
+    exports.ox_target:addGlobalPlayer({
+        options = {
+            {
+                onSelect = function()
+                    moneyMenu()
+                end,
+                icon = "fas fa-money",
+                label = "Give Money",
+            },
+        },
+        distance = 2.0
     })
 end)
