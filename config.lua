3653b43abd3a56944e3a44a9a176abbd685408cc
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-19 01:25:56 +0100

    Added option for qb menu

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 Config = {}
 
 Config.Target = 'qb' -- 'qb' or 'ox' depending on the target you use
+Config.Menu = 'ox' -- 'qb' or 'ox' depending on the menu you use
 Config.RangeOfRecipient = '10.0' -- reccomended to not change, but hey, do what you gotta do
