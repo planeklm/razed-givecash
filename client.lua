c7bbb52e2db8646945005f0ba6113df0f60af456
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-19 18:25:36 +0100

    Added message for recipient and fixed font icons.

--- client.lua
+++ client.lua
@@ -1,54 +1,71 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local TestTarget = Config.TestTarget
 
 local function moneyMenu()
-if Config.Menu == 'ox' then
-        local input = lib.inputDialog('Money Menu', {
-        {type = 'number', label = 'ID of recipent', description = 'The ID of the recipent.', default = '1', required = true, icon = 'user'},
-        {type = 'number', label = '$ Amount', description = 'The amount of money to send to the recipent.', default = '10', required = true, icon = 'money'}
-    })
+    if Config.Menu == 'ox' then
+            local input = lib.inputDialog('Money Menu', {
+            {type = 'number', label = 'ID of recipent', description = 'The ID of the recipent.', default = 1, min = '1', required = true, icon = 'user'},
+            {type = 'number', label = '$ Amount', description = 'The amount of money to send to the recipent.', default = 10, minimun = '1', required = true, icon = 'dollar-sign'},
+            {type = 'input', label = 'Message For Recipient', description = 'The message to send with the money to the recipent.', default = 'Here, have some cash.', minimun = '0', required = false, icon = 'message'}
+        })
 
     if not input[1] or not input[2] then return end
-        TriggerServerEvent("razed-givecash:server:chargerecipient", input[1], input[2])
+        if not input then return end
+             TriggerServerEvent("razed-givecash:server:chargerecipientOX", input[1], input[2], input[3])
     end
-else if Config.Target == 'qb' then
-    local input = exports['qb-input']:ShowInput({
+
+if Config.Menu == 'qb' then
+    local inputqb = exports['qb-input']:ShowInput({
         header = "Money Menu",
         submitText = "Confirm",
         inputs = {
             {
                 type = 'number',
                 isRequired = true,
-                name = 'input[1]',
+                name = 'idrecipient',
                 text = 'ID of recipent'
             },
             {
                 type = 'number',
                 isRequired = true,
-                name = 'input[2]',
+                name = 'mular',
                 text = '$ Amount'
             }
         }
     })
-    if input then
-        if not input.id or not input.amount then return end
-        TriggerServerEvent("tc-giveCash:server:charge", input.id, input.amount)
+    if inputqb then
+        if not inputqb.id or not inputqb.amount then return end
+        TriggerServerEvent("razed-givecash:server:chargerecipientQB", inputqb.id, inputqb.amount)
+        end
     end
 end
 
 CreateThread(function()
     if Config.Target == 'qb' then
-    exports['qb-target']:AddGlobalPlayer({
-        options = {
-            { 
-                action = function()
-                    moneyMenu()
-                end,
-                icon = "fas fa-money",
-                label = "Give Recipient Cash",
+        exports['qb-target']:AddGlobalPlayer({
+            options = {
+                { 
+                    action = function()
+                        moneyMenu()
+                    end,
+                    icon = "fas fa-money",
+                    label = "Give Recipient Cash",
+                },
             },
-        },
-        distance = 3.0 
-    })
+            distance = Config.TargetDistance
+        })
+    --[[exports['qb-target']:AddTargetModel(TestTarget, {
+        options = {
+        {
+            action = function()
+                moneyMenu()
+            end,
+            icon = "fas fa-money",
+            label = "Give Recipient Cash",
+        }
+      },
+      distance = 3.0,
+    })]]--
 else if Config.Target == 'ox' then
     exports.ox_target:addGlobalPlayer({
         options = {
@@ -60,6 +77,9 @@ else if Config.Target == 'ox' then
                 label = "Give Recipient Cash",
             },
         },
-        distance = 2.0
+        distance = 3.0
     })
+    end
+end
 end)
+
