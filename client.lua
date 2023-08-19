d8e70cbdc56ee34f367d89db668079c0d708507d
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-19 01:25:13 +0100

    Added option for qb menu

--- client.lua
+++ client.lua
@@ -1,13 +1,37 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function moneyMenu()
-    lib.inputDialog('Money Menu', {
+if Config.Menu == 'ox' then
+        local input = lib.inputDialog('Money Menu', {
         {type = 'number', label = 'ID of recipent', description = 'The ID of the recipent.', default = '1', required = true, icon = 'user'},
         {type = 'number', label = '$ Amount', description = 'The amount of money to send to the recipent.', default = '10', required = true, icon = 'money'}
     })
 
     if not input[1] or not input[2] then return end
-    TriggerServerEvent("razed-givecash:server:chargerecipient", input[1], input[2])
+        TriggerServerEvent("razed-givecash:server:chargerecipient", input[1], input[2])
+    end
+else if Config.Target == 'qb' then
+    local input = exports['qb-input']:ShowInput({
+        header = "Money Menu",
+        submitText = "Confirm",
+        inputs = {
+            {
+                type = 'number',
+                isRequired = true,
+                name = 'input[1]',
+                text = 'ID of recipent'
+            },
+            {
+                type = 'number',
+                isRequired = true,
+                name = 'input[2]',
+                text = '$ Amount'
+            }
+        }
+    })
+    if input then
+        if not input.id or not input.amount then return end
+        TriggerServerEvent("tc-giveCash:server:charge", input.id, input.amount)
     end
 end
 
