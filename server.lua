64c0da0e2f5c2b42c5ede6a2fd3900473c4d69b5
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-22 00:24:16 +0100

    Removed more QB-menu stuff

--- server.lua
+++ server.lua
@@ -44,24 +44,3 @@ RegisterNetEvent('razed-givecash:server:chargerecipientOX', function(id1, id2, i
         end
     end
 end)
-
-RegisterNetEvent('razed-givecash:server:chargerecipientQB', function(idrecipient, mular)
-    local payee = QBCore.Functions.GetPlayer(source)
-    local recipient = QBCore.Functions.GetPlayer(tonumber(idrecipient))
-    local recipientPed = GetPlayerPed(tonumber(idrecipient))
-    local payeePed = GetPlayerPed(source)
-
-    if not payee or not recipient then return end
-        local checkRangeOfRecip = #(GetEntityCoords(recipientPed) - GetEntityCoords(payeePed))
-        if checkRangeOfRecip < 10.0 then
-            if tonumber(mular) > 0 then
-                if payee.Functions.RemoveMoney('cash', tonumber(mular)) then
-                    recipient.Functions.AddMoney('cash', tonumber(mular))
-                    TriggerClientEvent('ox_lib:notify', source, 'Sent $'..tonumber(mular).." to"..idrecipient, 'success')
-                        TriggerClientEvent('ox_lib:notify', idrecipient, 'You have been given $' ,'success')
-                end
-        else
-            TriggerClientEvent('ox_lib:notify', source, 'Too far from recipient.', 'error')
-        end
-    end
-end)
